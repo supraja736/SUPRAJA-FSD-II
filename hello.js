2e460000f01ce5b2d120d6d87dc8a4ccaf1dbc78
supraja <suprajajavvadi2005@gmail.com>
2026-07-13 10:03:55 +0530

    my first ts code

---
+++ hello.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 34;
+console.log(age);
+let firstpro = ("Hello My first program");
+console.log(firstpro);
+//# sourceMappingURL=hello.js.map
---
+++ hello.ts
@@ -0,0 +1,6 @@
+let age:number=34;
+console.log(age);
+
+
+let firstpro:string=("Hello My first program");
+console.log(firstpro);
