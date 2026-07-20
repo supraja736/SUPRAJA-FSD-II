2a82386912c1ca2c8d9a1ea5ea4630adefd53611
supraja <suprajajavvadi2005@gmail.com>
2026-07-20 17:40:15 +0530

    Experiment*

---
+++ exp1(i).ts
@@ -0,0 +1,7 @@
+"use strict";
+let number = 100;
+let message = "Hello, TypeScript!";
+let isPass = true;
+console.log("Number:", number);
+console.log("String:", message);
+console.log("Boolean:", isPass);
